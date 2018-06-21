8ba55d0c8274a1c6e200fbe9f867134d46d23379
Roger Castro <omarcastro18@gmail.com>
2018-06-21 09:14:29 -0300

    constants that are going to be used in the game

---
+++ src/app/constants.js
@@ -0,0 +1,23 @@
+export const MINE_STATE_CLEAR = 0;
+export const MINE_STATE_MINE = 1;
+
+
+export const CELL_STATE_UNCLEARED = -1;
+export const CELL_STATE_FLAGGED = -2;
+export const CELL_STATE_HIT_MINE = -3;
+export const CELL_STATE_REVEAL_MINE_FOUND = -4;
+export const CELL_STATE_REVEAL_MINE_UNFOUND = -5;
+export const CELL_STATE_REVEAL_MINE_WRONG = -6;
+export const CELL_STATE_0 = 0;
+export const CELL_STATE_1 = 1;
+export const CELL_STATE_2 = 2;
+export const CELL_STATE_3 = 3;
+export const CELL_STATE_4 = 4;
+export const CELL_STATE_5 = 5;
+export const CELL_STATE_6 = 6;
+export const CELL_STATE_7 = 7;
+export const CELL_STATE_8 = 8;
+
+
+// proximity counts
+export const PROXIMITY_IS_MINE = -1;
