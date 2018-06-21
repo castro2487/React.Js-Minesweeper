b646139cdedfcfd906e0467cb20d4227e9150b9f
Roger Castro <omarcastro18@gmail.com>
2018-06-21 09:13:40 -0300

    js starting point

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app/components/App';
+import './assets/styles/styles.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
